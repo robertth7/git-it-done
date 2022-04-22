8810e09321194927727594e107753d609540c70d
Roberto Hurtado Jr <rhurtado.267@gmail.com>
2022-04-22 10:12:09 -0500

    newly reformed 6.1

--- module-6-starter/assets/js/homepage.js
+++ module-6-starter/assets/js/homepage.js
@@ -1,5 +1,32 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        console.log(response);
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
-getUserRepos();
+// var userFormEl = document.querySelector("#user-form");
+// var nameInputEl = document.querySelector("#username");
+
+// var formSubmitHandler = function(event) {
+//     event.preventDefault();
+//     console.log(event);
+// };
+// var username = nameInputEl.value.trim();
+
+// if (username) {
+//     getUserRepos(username);
+//     nameInputEl.value = "";
+// } else {
+//     alert("Please enter a GitHub username");
+// }
+
+
+// userFormEl.addEventListener("submit", formSubmitHandler);
+getUserRepos("robertth7");
