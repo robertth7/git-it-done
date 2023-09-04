fa8a9a2e060370b7da0f91887075e5205716d065
Roberto Hurtado Jr <rhurtado.267@gmail.com>
2023-09-04 17:01:25 -0500

    adding buttons

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,5 +1,6 @@
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user) {
     // format that github api url
@@ -88,4 +89,33 @@ var displayRepos = function(repos, searchTerm) {
     }
 };
 
+// create a function that accepts a language parameter, creates an API endpoint, and makes an HTTP request to that endpoint using fetch()
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response){
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+};
+
+var buttonClickHandler = function(event){
+    var language = event.target.getAttribute("data-language");
+    console.log(language);
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content of text from repo container
+        repoContainerEl.textContent = "";
+    }
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+// add event delegation. to keep code DRY, we can delegate click handling on these elements to their parents elements. 
+languageButtonsEl.addEventListener("click", buttonClickHandler);
